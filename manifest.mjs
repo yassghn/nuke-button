62811265d45f24f01a7911c9aa4868cd87b6fbed
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-03-12 22:49:25 -0500

    use mjs file for userscript manifest

---
+++ manifest.mjs
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name        nuke button
+// @namespace   https://github.com/yassghn/nuke-button
+// @version     2025-02-28
+// @description kill 'em all
+// @downloadURL https://update.greasyfork.org/scripts/528380/nuke%20button.user.js
+// @updateURL   https://update.greasyfork.org/scripts/528380/nuke%20button.meta.js
+// @icon        https://www.svgrepo.com/download/528868/bomb-emoji.svg
+// @author      yassghn
+// @match       https://twitter.com/*
+// @match       https://mobile.twitter.com/*
+// @match       https://x.com/*
+// @match       https://mobile.x.com/*
+// @run-at      document-start
+// @grant       none
+// @license     OUI
+// @require     https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js
+// ==/UserScript==
+
+/* global $ */
